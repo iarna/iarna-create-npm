a1df92ea1ed307323bee524b66bfc00313fb3908
Rebecca Turner <me@re-becca.org>
2018-06-22 16:06:58 -0700

    feat(scopes): Explicitly support npmcorp and npmscopes in non @-prefixed dir names

--- init.js
+++ init.js
@@ -4,6 +4,7 @@ const initIt = require('@iarna/create')
 
 initIt(process.cwd(), `${__dirname}/node_modules/@iarna/create/template`, {
   github: 'npm',
-  copyright: 'npm, Inc'
+  copyright: 'npm, Inc',
+  namespaces: ['npmcorp', 'npm']
 })
 console.log('* project initialized')
